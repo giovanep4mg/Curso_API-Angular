8e03f3783c2f12ac4d3e24092d59fe7764e3db28
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-04-01 08:25:47 -0300

    Curso_Api-Angular-aula2E

---
+++ php/conexao.php
@@ -0,0 +1,14 @@
+<?php
+// Para ter acesso ao banco de dados, o "api".
+
+
+// Variáveis
+$url = "localhost";
+$usuario = "root";
+$senha = "" ;
+$base = "api";
+
+//conexão
+$conexao = mysqli_connect($url, $usuario, $senha, $base );
+
+?>
---
+++ php/listar.php
@@ -0,0 +1,41 @@
+<?php
+// exibir um JSon
+
+// incluir a conexão
+include("conexao.php");
+
+
+// sql
+$sql = " SELECT * FROM cursos "; 
+
+
+// executar
+$executar = mysqli_query($conexao, $sql);
+
+// vetor
+$cursos = [] ;
+
+// indice 'qual posição'
+$indice = 0 ;
+
+// laço
+while($linha = mysqli_fetch_assoc($executar)) {
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+
+    $indice++;
+
+}
+
+//guarda no JSON
+json_encode(['cursos' => $cursos]);
+
+
+
+
+
+
+
+
+?>
