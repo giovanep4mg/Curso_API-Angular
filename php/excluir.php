aa5a3ac84e27f9091afb032a83383c5c7ccbf811
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-04-01 19:35:42 -0300

    Aula-2-H

---
+++ php/excluir.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// incluir a conexão
+include("conexao.php");
+
+// obter dados "dados que serão enviados"
+$obterDados = file_get_contents("php:// input");
+
+// extrair os dados do JSON
+$extrair = json_decode($obterDados);
+
+// separar os dados do JSON
+$idCurso = $extrair->cursos->idCurso;
+
+// sql
+$sql = " DELETE FROM cursos Where idCurso = $idCurso";
+mysqli_query($conexao, $sql);
+
+
+?>
