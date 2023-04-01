e3dc033bd3db44bdc8c94292c9514f9ced974fdc
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-04-01 19:30:28 -0300

    Aula-2-G

---
+++ php/alterar.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// incluir a conexão
+include("conexao.php");
+
+// obter dados "dados que serão enviados"
+$obterDados = file_get_contents("php:// input");
+
+// extrair os dados do JSON
+$extrair = json_decode($obterDados);
+
+// separar os dados do JSON
+$idCurso = $extrair->cursos->idCurso;
+$nomeCurso = $extrair->cursos->nomeCurso;
+$valorCurso = $extrair->cursos->valorCurso;
+
+// sql
+$sql = " UPDATE cursos SET nomeCursos = '$nomeCurso', valorCurso = $valorCurso Where idCurso = $idCurso";
+mysqli_query($conexao, $sql);
+
+
+
+// exporta os dados cadatrados
+$curso = [
+    'idCurso' => $idCurso,
+    'nomeCurso' => $nomeCurso,
+    'valorCurso' =>  $valorCurso
+]
+
+json_encode(["curso"] => $curso);
+
+
+
+?>
