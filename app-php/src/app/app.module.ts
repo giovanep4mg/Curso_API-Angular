5b2d73b667762cfcaebe4b0053f95f8dd9c9c8d8
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-04-24 17:44:44 -0300

    aula-3-E-Explicando-Componente

--- app-php/src/app/app.module.ts
+++ app-php/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { CursoComponent } from './curso/curso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent
   ],
   imports: [
     BrowserModule,
