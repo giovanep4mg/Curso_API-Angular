9d016c4f437ef72f1365d3322f80738b52ab2498
Giovani teodoro Martins <giovaneteodoro79@gmail.com>
2023-05-11 17:52:41 -0300

    atualizando-bloco-notas

--- php/conexao.php
+++ php/conexao.php
@@ -7,17 +7,15 @@ $usuario = "root";
 $senha = "" ;
 $base = "api";
 
-
 //conexão
 $conexao = mysqli_connect($url, $usuario, $senha, $base );
 
+// verificar se a conexão foi realizada ou não, se não foi será exibida essa mensagem.
 if (!$conexao) {
     die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
   }
 
-
 // arrumar caracteres especiais
 mysqli_set_charset($conexao, "utf8");
-
 
 ?>
--- php/listar.php
+++ php/listar.php
@@ -1,43 +1,36 @@
 <?php
-// exibir um JSon
+// cria um json com todos os cursos
 
 // incluir a conexão
 include("conexao.php");
 
-
-
-// sql
+// o que vai fazer no banco de dados "api"
 $sql = " SELECT * FROM cursos "; 
 
-
-
-// executar
+// "mysqli_query()" vai executar os parâmetros passados dentro do parênteses.
 $executar = mysqli_query($conexao, $sql);
 
-
-
-// vetor
+// $cursos => vai guarda um vetor, uma lista de todos os cursos
 $cursos = [] ;
-
 
 // indice 'qual posição'
 $indice = 0 ;
 
-// laço
+// laço repetir isso até o indice ser maior que o real.
 while($linha = mysqli_fetch_assoc($executar)) {
     $cursos[$indice]['idCurso'] = $linha['idCurso'];
     $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
     $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
 
     $indice++;
-
 }
+// variável curso, vai seguir o indice , passando e pegando os dados e salvando na variável $cursos.
 
-//guarda no JSON
-//json_encode(['cursos' => $cursos]);
-
+ // para ser exibido no "http://localhost/api/php/listar.php" 
+ // mas deve ser desativado para não dá erro no código.
  // var_dump($curso) //nao está sendo listada
 
+ // cria um json, que será transmitido para o front-end.
 echo  json_encode($cursos)
 
 ?>
